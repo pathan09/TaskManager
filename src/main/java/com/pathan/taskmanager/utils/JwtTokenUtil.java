b9567aba1ad282929f6946d821fcc2c67e8d037a
Pathan <a.p.sust@gmail.com>
2021-02-21 13:16:25 +0600

    Update Token

--- src/main/java/com/pathan/taskmanager/utils/JwtTokenUtil.java
+++ src/main/java/com/pathan/taskmanager/utils/JwtTokenUtil.java
@@ -18,7 +18,7 @@ public class JwtTokenUtil implements Serializable {
 
 	private static final long serialVersionUID = -2550185165626007488L;
 	
-	public static final long JWT_TOKEN_VALIDITY = 3*60;
+	public static final long JWT_TOKEN_VALIDITY = 30*60;
 	public static final long JWT_REFRESH_TOKEN = 5*60*60;
 
 	@Value("${jwt.secret}")
